54a26eb2ea759987c1e4a436c1d0ae62891abe50
Grzegorz Janiszewski <zamowienia@biurotfc.pl>
2017-10-07 13:35:24 +0200

    a href zaczynajace sie http w nav

--- scripts.js
+++ scripts.js
@@ -5,8 +5,13 @@
             //wszystkie elementy <div>, posiadające zarówno klasę “grid” oraz klasę “grid-12”
             var grid = $("div.grid, div.grid-12");
 
-            console.log(grid);
+            console.log("div grid grid-12",grid);
+
+            //wszystkie elementy <a>, których atrybut “href" zaczyna się na “http”, znajdujące się w elemencie o klasie “nav”
+            var aHttp = $(".nav a[href^='http']");
     
+            console.log("a zaczynające się http",aHttp);
+
         });
     
     })(jQuery);
