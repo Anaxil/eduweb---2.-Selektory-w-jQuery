ffe175f82b8bc7e7a75a09ce3d6272f2cb509ed0
Grzesiek <jajosc@poczta.fm>
2017-10-07 13:05:24 +0200

    dodano plik scripts.js z selektorem div grid i grid-12

---
+++ scripts.js
@@ -0,0 +1,14 @@
+(function($) {
+    
+        $(document).ready(function() {
+    
+            //wszystkie elementy <div>, posiadające zarówno klasę “grid” oraz klasę “grid-12”
+            var grid = $("div.grid, div.grid-12");
+
+            console.log(grid);
+    
+        });
+    
+    })(jQuery);
+    
+    
